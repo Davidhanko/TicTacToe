6c8246a34933ebb6b554cf21b3eedbd8a2c8fdf8
Davihan11 <davihan11@gmail.com>
2023-02-02 17:16:17 +0100

    Javascript code

--- js.js
+++ js.js
@@ -1,24 +1,23 @@
-const gameBoard = {
-  board: ["", "", "", "", "", "", "", "", ""],
+const gameBoard = () => {
+  const board = ["", "", "", "", "", "", "", "", ""];
+  const getBoard = () => board;
+  const setBoard = (index, value) => (board[index] = value);
+  return { getBoard, setBoard };
 };
 
 const player = (name, symbol) => {
-  return { name, symbol };
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  return { getName, getSymbol };
 };
 
-const game = (() => {
-  const startGame = () => {
-    const player1Name = prompt("Player 1 name: ");
-    const player1Symbol = prompt("Player 1 symbol: ");
-    const player2Name = prompt("Player 2 name: ");
-    const player2Symbol = prompt("Player 2 symbol: ");
-    const player1 = player(player1Name, player1Symbol);
-    const player2 = player(player2Name, player2Symbol);
+displayController = () => {
+  const displayBoard = () => {
+    const board = gameBoard.getBoard();
+    for (let i = 0; i < board.length; i++) {
+      const cell = document.getElementById(i.toString());
+      cell.textContent = board[i];
+    }
   };
-
-  return { startGame };
-})();
-
-const displayController = (() => {})();
-
-game.startGame();
+  return { displayBoard };
+};
