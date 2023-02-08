2bb948c10d4c3efb0bf6095fdec30c1231ad1657
Davihan11 <davihan11@gmail.com>
2023-02-08 21:53:17 +0100

    Added little logic

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
+let lastMove = "O";
+let playerTurn = "X";
+
 const gameBoard = (() => {
   const board = [
     ["", "", ""],
@@ -5,7 +8,6 @@ const gameBoard = (() => {
     ["", "", ""],
   ];
   const htmlBoard = document.getElementsByClassName("board");
-  console.log(htmlBoard);
   const getBoard = () => board;
   const setBoard = (index, index2, value) => (board[index][index2] = value);
   return { htmlBoard, getBoard, setBoard };
@@ -14,6 +16,7 @@ const gameBoard = (() => {
 const player = (name, symbol) => {
   const getName = () => name;
   const getSymbol = () => symbol;
+
   return { getName, getSymbol };
 };
 
@@ -22,13 +25,59 @@ const displayController = (() => {
     let board = gameBoard.getBoard();
     for (let i = 0; i < board.length; i++) {
       for (let j = 0; j < board[i].length; j++) {
-        console.log(gameBoard.htmlBoard[0]);
-        gameBoard.htmlBoard[i].children[j].textContent = board[i][j];
+        gameBoard.htmlBoard[i].children[j].addEventListener("click", (e) => {
+          const element = e.target;
+          const symbol = gameLogic.displaySymbol();
+          showSymbol(symbol, element);
+          gameBoard.setBoard(i, j, symbol);
+        });
       }
     }
   };
+  const showSymbol = (symbol, element) => {
+    element.textContent = symbol;
+  };
   return { displayBoard };
 })();
-gameBoard.setBoard(0, 1, "X");
 
-displayController.displayBoard();
+const gameStarter = (() => {
+  const start = () => {
+    displayController.displayBoard();
+  };
+  return { start };
+})();
+
+const gameLogic = (() => {
+  const PlayerTurnFunc = () => {
+    PlayerTurnSystem.checkAndChangePlayer();
+  };
+  const displaySymbol = () => {
+    if (playerTurn === "X") {
+      PlayerTurnFunc();
+      console.log("test");
+      return "X";
+    } else {
+      PlayerTurnFunc();
+      console.log("testing");
+      return "O";
+    }
+  };
+  return { PlayerTurnFunc, displaySymbol };
+})();
+
+const PlayerTurnSystem = (() => {
+  const checkAndChangePlayer = () => {
+    if (lastMove === "O") {
+      lastMove = "X";
+      playerTurn = "O";
+    } else {
+      lastMove = "O";
+      playerTurn = "X";
+    }
+  };
+  return { checkAndChangePlayer };
+})();
+
+gameStarter.start();
+const player1 = player("Player 1", "X");
+const player2 = player("Player 2", "O");
